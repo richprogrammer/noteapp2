2f6b10d6abdbe0db4eb14b303840c37755e6dc87
Rich <richardleelovestoprogram@gmail.com>
2024-04-08 06:21:17 -0400

    update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 #Creating the main page
+@app.route("/")
 def main():
-    return render_template()
+    return render_template("index.html")
 
 #Running the app
 if __name__ == '__main__':
