d8b39864dbdd015d8c2193af2e5eedca71963526
Rich <richardleelovestoprogram@gmail.com>
2024-04-20 09:19:09 -0400

    update main.py

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from flask import Flask, render_template, request, url_for
 #Creating the app
 app = Flask(__name__)
 
+#Global Variables
+notes_list = []
+
 #Creating the main page
 @app.route("/", methods=["GET", "POST"])
 def main():
+    global notes_list
+
     if request.method == "POST":
         webpage = request.form
         notes_inp = str(webpage['notesInp'])
         print(notes_inp)
+        notes_list.append(notes_inp)
 
     return render_template("index.html")
 
